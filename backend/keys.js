d8631406bcf8a2f4231942eace53252ad910d789
Bmokshith2406 <151653709+Bmokshith2406@users.noreply.github.com>
2024-10-21 18:36:18 +0530

    Update keys.js

--- backend/keys.js
+++ backend/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongourl:
-    "mongodb://hellodear7931:Projectsanta@ac-pq81fdn-shard-00-00.m3zngqn.mongodb.net:27017,ac-pq81fdn-shard-00-01.m3zngqn.mongodb.net:27017,ac-pq81fdn-shard-00-02.m3zngqn.mongodb.net:27017/?ssl=true&replicaSet=atlas-3nsa5l-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Instagram",
+    "mongodb://localhost:27017/instagram-clone",
   JWT_SecKey: "Edookati11012005",
 };
