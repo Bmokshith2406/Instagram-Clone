2d1dcc09ae0ca7b089fd697af8df4e204ed368a2
Bmokshith2406 <151653709+Bmokshith2406@users.noreply.github.com>
2024-10-21 18:31:53 +0530

    Update keys.js

--- backend/keys.js
+++ backend/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongourl:
     "mongodb://hellodear7931:Projectsanta@ac-pq81fdn-shard-00-00.m3zngqn.mongodb.net:27017,ac-pq81fdn-shard-00-01.m3zngqn.mongodb.net:27017,ac-pq81fdn-shard-00-02.m3zngqn.mongodb.net:27017/?ssl=true&replicaSet=atlas-3nsa5l-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Instagram",
-  JWT_SecKey: "AarusSanHarVrit2004",
+  JWT_SecKey: "Edookati11012005",
 };
